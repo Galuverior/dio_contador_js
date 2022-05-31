460fc97a1978f9385ad0cb6ea8240c13ae80608e
Eduardo Fragoso <duds.fragoso@gmail.com>
2022-05-30 23:45:02 -0300

    criação do contador

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,52 @@
-var currentNumberWrapper = document.getElementById('currentNumber');
-var currentNumber = 0;
+// Total count
+const totalCount = document.getElementById('currentNumber')
 
-function increment() {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-}
+// Add/Subtract Buttons
+const subtractNumber = document.getElementById('subtract')
+const addNumber = document.getElementById('add')
 
-function decrement() {
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-}
+const text = document.querySelector('.text')
+const text2 = document.querySelector('.text2')
+
+// Variable to track count
+var currentNumber = 0
+
+// Display initial value
+totalCount.innerHTML = currentNumber
+
+// Add click event and function to buttons
+subtractNumber.addEventListener('click', function () {
+  currentNumber--
+  if (currentNumber < 0) {
+    totalCount.style.color = 'red'
+  }
+  if (currentNumber == -5) {
+    totalCount.innerHTML = currentNumber
+    text2.classList.add('show')
+    subtractNumber.disabled = true
+  }
+  if (currentNumber < 5) {
+    addNumber.disabled = false
+    totalCount.innerHTML = currentNumber
+    text.classList.remove('show')
+  }
+  totalCount.innerHTML = currentNumber
+})
+
+addNumber.addEventListener('click', function () {
+  currentNumber++
+  if (currentNumber == 5) {
+    totalCount.innerHTML = currentNumber
+    text.classList.add('show')
+    addNumber.disabled = true
+  }
+  if (currentNumber > -5) {
+    totalCount.innerHTML = currentNumber
+    text2.classList.remove('show')
+    subtractNumber.disabled = false
+  }
+  if (currentNumber >= 0) {
+    totalCount.style.color = 'black'
+  }
+  totalCount.innerHTML = currentNumber
+})
